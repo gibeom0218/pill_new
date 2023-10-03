e51af2ca70f640d11e075cf014d1f3ff653c4349
JaeHo <l2281@naver.com>
2023-10-03 16:12:06 +0900

    2023-10-03 JH

--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import TextField from '@mui/material/TextField';
 import Checkbox from '@mui/material/Checkbox';
@@ -11,8 +11,35 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import axios from 'axios';
 
 export default function App() {
+  const [formData, setFormData] = useState({
+    id: '',
+    password: ''
+  });
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // 사용자의 입력 데이터를 formData 객체에서 가져와서 서버로 전송
+      const response = await axios.post('http://110.12.181.206:8081/login', formData);
+      console.log('서버 응답: ', response.data);
+
+      // 성공적으로 로그인 후 필요한 작업을 수행하세요
+    } catch (error) {
+      // 오류 처리
+      console.error('로그인 오류: ', error);
+    }
+  };
   return (
     <Container component="main" maxWidth="xs">
        <Box
@@ -28,20 +55,30 @@ export default function App() {
         </Avatar>
        <Typography component="h1" variant="h5">
             Sign in
-          </Typography>
-      <TextField
-        margin = "normal" 
-        label="ID"  
-        required 
-        name="id"/> 
-      <TextField
-        margin = "normal"  
-        label="Password" 
-        type="password" 
-        required
-        name="password"/>
-      <FormControlLabel control={ <Checkbox value="remember" color="primary" />} label="Remember me"/>
-      <Button type="submit" fullWidth variant="contained" sx={{mt:3, mb:2}}>Sign in</Button>
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            margin="normal"
+            label="ID"
+            required
+            name="id"
+            value={formData.id}
+            onChange={handleFormChange}
+            sx={{ width: '70%' }} // TextField의 폭을 100%로 지정하여 정렬
+          />
+          <TextField
+            margin="normal"
+            label="Password"
+            type="password"
+            required
+            name="password"
+            value={formData.password}
+            onChange={handleFormChange}
+            sx={{ width: '70%' }} // TextField의 폭을 100%로 지정하여 정렬
+          />
+          <FormControlLabel control={ <Checkbox value="remember" color="primary" />} label="Remember me"/>
+          <Button type="submit" fullWidth variant="contained" sx={{mt:3, mb:2}}>Sign in</Button>
+      </form>
       <Grid container>
        <Grid item xs>
         <Link>Forgot Password?</Link>
--- src/pillSearch.js
+++ src/pillSearch.js
@@ -3,23 +3,25 @@ import axios from 'axios';
 import { TextField, Button, Typography, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 
 const PillSearchPage = () => {
-  const [searchTerm, setSearchTerm] = useState(''); // 약 이름 검색을 위한 입력 필드의 값을 저장
-  const [searchResults, setSearchResults] = useState([]); // 검색된 약 리스트를 저장
-  const [registerStatus, setRegisterStatus] = useState(''); //약 등록 상태를 저장
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [registerStatus, setRegisterStatus] = useState('');
 
-  const handleSearchTermChange = (event) => { //약 이름 검색 입력 필드의 변경을 감지하고, searchTerm 상태를 업데이트.
+  const handleSearchTermChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearch = async (event) => { //검색 버튼을 클릭하면 호출되는 함수로, Axios를 사용하여 서버에 약 검색 요청을 보냄.
-    event.preventDefault(); //서버에 GET 요청을 보내고, 검색된 약 리스트를 searchResults 상태로 업데이트합니다.
-
+  const handleSearch = async (event) => {
+    event.preventDefault();
+  
     try {
-      const response = await axios.get(`/pillSearch?pillName=${searchTerm}`);
+      const response = await axios.get(`http://110.12.181.206:8081/pillSearch?pillName=${searchTerm}`);
+      console.log('Server Response:', response.data);
       const { success, pillList, errorMessage } = response.data;
-
+      setSearchResults(pillList);
+  
       if (success) {
-        setSearchResults(pillList);
+        setSearchResults(pillList); // Set the pillList directly
       } else {
         console.error('Search failed:', errorMessage);
       }
@@ -28,14 +30,14 @@ const PillSearchPage = () => {
     }
   };
 
-  const handleRegister = async (pillCode) => { // 약을 등록하기 위해 호출되는 함수로, 약을 선택하고 "등록" 버튼을 클릭하면 호출.
-    const data = { //서버에 POST 요청을 보내고, 약 등록 상태를 registerStatus 상태로 업데이트.
-      id: 'example_id', // Example: Replace with actual user ID
+  const handleRegister = async (pillCode) => {
+    const data = {
+      id: 'example_id',
       pillCode,
     };
 
     try {
-      const response = await axios.post('/pillEnroll', data);
+      const response = await axios.post('http://110.12.181.206:8081/pillSearch', data);
       const { success, successMessage, errorMessage } = response.data;
 
       if (success) {
@@ -70,15 +72,18 @@ const PillSearchPage = () => {
 
       <div style={{ marginTop: '20px' }}>
         <Typography variant="h5">검색된 약 리스트</Typography>
-        <List>     
-          {searchResults.map((pill, index) => (
-            <ListItem key={index}>
-              <ListItemButton onClick={() => handleRegister(pill.pillCode)}>
-                <ListItemText primary={pill.pillName} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
+        <List>
+        {searchResults.map((pill, index) => (
+          <ListItem key={index}>
+            <ListItemButton onClick={() => handleRegister(pill.pillCode)}>
+              <ListItemText
+                primary={pill.pillName}
+                secondary={`약 코드: ${pill.pillCode}`}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
       </div>
 
       <div style={{ marginTop: '20px' }}>
@@ -90,8 +95,3 @@ const PillSearchPage = () => {
 };
 
 export default PillSearchPage;
-
-/*Axios를 사용하여 HTTP 요청을 보내고, 서버로부터 응답을 받음.
- 검색 및 등록 요청에 대한 응답을 처리하고 상태를 업데이트.
-이 컴포넌트는 약 검색, 약 등록, 그리고 관련된 상태 및 이벤트 처리를 통합하여 약 검색 및 등록 페이지를 구성. 
-사용자가 검색한 약을 선택하여 등록할 수 있도록 UI를 제공. */
