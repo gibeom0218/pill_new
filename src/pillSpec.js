a3033a712b9040bd4fdf254aaf8a910f9e1cf16c
doyun <youn6432@naver.com>
2023-10-11 23:07:29 +0900

    2023-10-11 dy1

--- src/pillSpec.js
+++ src/pillSpec.js
@@ -1,32 +1,35 @@
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 function PillSpec() {
-
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
+  const { pillName } = useParams(); // useParams 훅을 사용하여 URL에서 pillCode를 가져옵니다.
 
   useEffect(() => {
     // 서버에서 데이터를 가져오는 요청
-    axios.get('https://api.example.com/pillCodes') // 실제 엔드포인트로 대체해야 합니다.
+    axios.get(`http://110.12.181.206:8081/pillSpec/?pillName=${pillName}`)
       .then(response => {
-        // 서버에서 받아온 데이터를 상태에 저장
+        console.log(response.data);// 서버에서 받아온 데이터를 상태에 저장
         setData(response.data);
       })
       .catch(error => {
         console.error('데이터를 받아오는 중 오류 발생:', error);
       });
-  }, []);
+  }, [pillName]); // pillCode가 변경될 때마다 useEffect가 다시 실행됩니다.
 
-  // useParams 훅을 사용하는 예시
-  const { pillCode } = useParams();
-
-  // 컴포넌트 내용 반환
   return (
     <div>
-      <h2>Pill Spec 페이지 - {pillCode}</h2>
-      <p>약 이름: {data.name}</p>
+      
+      <p><h2>약 이름</h2> {data.itemName}</p>
+      <h2>주의사항</h2>{data.atpnQesitm} <p></p>
+      <h2>병용금기 </h2>{data.intrcQesitm}
+      <h2>효능효과</h2>{data.efcyQesitm}
+      <h2>복용방법</h2>{data.useMethodQesitm}
+      <h2>부작용</h2>{data.seQesitm}
+      <h2>보관방법</h2>{data.depositMethodQesitm}
+
+
       
     </div>
   );
